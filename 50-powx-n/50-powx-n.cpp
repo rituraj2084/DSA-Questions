bf3c85c39ab959289f362b5611c24af8093c5ee0
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-06-08 14:46:25 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.30%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,7 +1,45 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+        long long nn = n;
+        double ans = 1.0;
+        if(nn<0){
+            nn *= -1;
+        }
+        while(nn > 0){
+           if(nn % 2 == 0){
+               x = x*x;
+               nn /= 2;
+           } 
+            if(nn % 2 != 0){
+                ans = ans * x;
+                nn -= 1;
+            }
+        }
+        if(n < 0){
+            ans = 1 / ans;
+        }
+        return ans;
+        
+        /* //using recursion
+        if(n>=0){
+            if(n == 0){
+            return 1;
+            }
+            return x*myPow(x, n-1);
+        }
+        else{
+            n *= -1;
+            double ans = x*myPow(x, n-1);
+            ans = 1/ans;
+            return ans;
+        }
+        */
+        
+        /*
+        //using in-built function
         double ans = pow(x, n);
         return ans;
+        */
     }
 };
