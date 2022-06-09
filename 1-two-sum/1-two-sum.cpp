8db1c5cca69e000e27acfd6bf6d40100fa9c8446
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-06-09 16:50:50 +0530

    Time: 12 ms (84.53%), Space: 10.5 MB (59.07%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> dummy;
+        dummy = nums;
+        sort(dummy.begin(), dummy.end());
+        int l = 0, h = nums.size()-1;
+        int n1,n2;
+        vector<int> ans;
+        while(l<h){
+            if(dummy[l] + dummy[h] == target){
+                n1 = dummy[l];
+                n2 = dummy[h];
+                break;
+            }
+            else if(dummy[l] + dummy[h] > target){
+                h--;
+            }
+            else{
+                l++;
+            }
+        }
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == n1){
+                ans.push_back(i);
+            }
+             else if(nums[i] == n2){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+        /*
+        //brute force
         int n = nums.size();
         vector<int> ans;
         for(int i = 0; i < n; i++){
@@ -12,5 +42,6 @@ public:
             }
         }
         return ans;
+        */
     }
 };
