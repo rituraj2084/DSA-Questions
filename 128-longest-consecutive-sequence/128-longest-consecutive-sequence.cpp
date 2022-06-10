afcd1c0364295578e6cbe542c2f2dd49ec02788c
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-06-10 16:39:13 +0530

    Time: 148 ms (66.36%), Space: 33.6 MB (60.55%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        
         if(nums.size() == 0){
             return 0;
         }
@@ -19,5 +20,26 @@ public:
             ans = max(ans, cur);
         }
         return ans;
+        
+        /*
+        set<int>hashSet;
+        for(int num: nums){
+            hashSet.insert(num);
+        }
+            int longestStreak = 0;
+            for(int num: nums){
+                if(!hashSet.count(num-1)){
+                    int currentNum = num;
+                    int currentStreak = 1;
+                    while(hashSet.count(currentNum+1)){
+                        currentNum += 1;
+                        currentStreak += 1;
+                    }
+                    longestStreak = max(longestStreak, currentStreak);
+                }
+            }
+        return longestStreak;
+        */
+        
     }
 };
