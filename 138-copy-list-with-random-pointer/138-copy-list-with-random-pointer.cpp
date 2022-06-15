32c114d41c6a1e493550f6beef75700ebc9a743d
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-06-15 19:18:47 +0530

    Time: 11 ms (69.93%), Space: 11.2 MB (87.69%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,6 +17,64 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        //STEP 1
+        if(!head) return NULL; 
+        Node* temp = head;
+        while(temp){ // while temp is not null
+            Node* node = new Node(temp->val);
+            node->next = temp->next; 
+            temp->next = node;
+            temp = node->next;
+        }
+		
+		//Step 2
+        temp = head; 
+        while(temp){
+            temp->next->random = (temp->random)?temp->random->next:NULL; 
+            temp = temp->next->next; 
+        }
+		
+		//Step 3
+        Node* final = head->next;
+        temp = head->next;
+        while(head){ 
+            head->next = temp->next;
+            head = head->next; 
+            if(!head) break; 
+            temp->next = head->next; 
+            temp = temp->next;
+        }
+        return final;
+        /*
+        if(head == NULL){
+            return NULL;
+        }
+        Node* temp = head;
+        while(temp){
+            Node* newNode = new Node(temp -> val);
+            newNode -> next = temp -> next;
+            temp -> next = newNode;
+            temp = newNode -> next;
+        }
+        temp = head;
+        while(temp){
+            temp -> next -> random = (temp -> random)?temp -> random -> next:NULL;
+            temp -> next = temp -> next -> next;
+        }
+        Node* ans = head -> next;
+        temp = head -> next;
+        while(head){
+            head -> next = temp -> next;
+            head = head -> next;
+            if(!head){
+                break;
+            }
+            temp -> next = head -> next;
+            temp = temp -> next;
+        }
+        return ans;
+        */
+        /*
        map<Node*, Node*> mp;
         Node* temp = head;
         while(temp){
@@ -31,5 +89,6 @@ public:
             temp = temp -> next;
         }
         return mp[head];
+        */
     }
 };
