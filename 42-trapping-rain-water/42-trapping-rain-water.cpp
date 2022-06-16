82a79b357ae278a4b292b6e3b4f3835902012667
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-06-16 15:17:10 +0530

    Time: 29 ms (23.46%), Space: 19.8 MB (34.70%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -2,6 +2,32 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
+        int left = 0, right = n - 1;
+        int leftMax = 0, rightMax = 0;
+        int ans = 0;
+        while(left <= right){
+            if(height[left] <= height[right]){
+                if(height[left] >= leftMax){
+                    leftMax = height[left];
+                }
+                else{
+                    ans += leftMax -height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right] >= rightMax){
+                    rightMax = height[right];
+                }
+                else{
+                    ans += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return ans;
+        /*
+        int n = height.size();
         int prefix[n];
         int suffix[n];
         prefix[0] = height[0], suffix[n - 1] = height[n - 1];
@@ -16,6 +42,7 @@ public:
             waterTrapped += min(suffix[i], prefix[i]) - height[i];
         }
         return waterTrapped;
+        */
         /*
       int n = height.size();
         int waterTrapped = 0;
