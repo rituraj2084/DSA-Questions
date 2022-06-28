753bfea94736daae111cd5e91152a97ef8da02d3
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-06-28 15:27:28 +0530

    Time: 9 ms (68.96%), Space: 6.7 MB (56.27%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
+    string say(string s){
+        string ans = "";
+        for(int i = 0; i < s.size(); i++){
+            int count = 1;
+            while(i+1 < s.size() && s[i] == s[i+1]){
+                i++;
+                count++;
+            }
+            ans+=(to_string(count)+s[i]);
+        }
+        return ans;
+    }
     string countAndSay(int n) {
+        if(n == 1) return "1";
+        return say(countAndSay(n-1));
+        /*
       string res = "1";
     for (int i=1;i<n;i++)
     {
@@ -19,6 +34,7 @@ public:
        temp += to_string(ct) + res[res.length()-1];
        res = temp;
     }
-    return res;  
+    return res; 
+    */
     }
 };
