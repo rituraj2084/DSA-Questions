b39548b8d0c84570ef9fb87ce054667973c5d664
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-06-30 02:08:28 +0530

    Added solution - LeetHub

--- Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.cpp
+++ Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.cpp
@@ -96,37 +96,57 @@ Node* buildTree(string str)
 class Solution {
   public:
     vector <int> bottomView(Node *root) {
-        // Your Code Here
-        vector<int> ans;
-        if(root == NULL) 
-        {
-            return ans;
-        }
-        
-        map<int,int> topNode;
-        queue<pair<Node*, int> > q;
-        
-        q.push(make_pair(root, 0));
-        
+        vector<int> ans; 
+        if(root == NULL) return ans; 
+        map<int,int> mpp; 
+        queue<pair<Node*, int>> q; 
+        q.push({root, 0}); 
         while(!q.empty()) {
-            pair<Node*, int> temp = q.front();
-            q.pop();
-            Node* frontNode = temp.first;
-            int hd = temp.second;
+            auto it = q.front(); 
+            q.pop(); 
+            Node* node = it.first; 
+            int line = it.second; 
+            mpp[line] = node->data; 
             
-            topNode[hd] = frontNode -> data;
-                
-            if(frontNode->left)
-                q.push(make_pair(frontNode->left, hd-1));
-            if(frontNode->right)
-                q.push(make_pair(frontNode->right, hd+1));
+            if(node->left != NULL) {
+                q.push({node->left, line-1}); 
+            }
+            if(node->right != NULL) {
+                q.push({node->right, line + 1}); 
+            }
+            
         }
         
-        for(auto i:topNode) 
-        {
-            ans.push_back(i.second);
+        for(auto it : mpp) {
+            ans.push_back(it.second); 
         }
         return ans;
+        /*
+        vector<int> ans;
+        if(root == NULL) return ans;
+        queue<pair<Node*, int>>q;
+        map<int, int>mp;
+        q.push({root, 0});
+        while(!q.empty()){
+            
+            auto it = q.front();
+            q.pop();
+            Node* node = it.first;
+            int line = it.second;
+            mp[line] = node -> data;
+            if(node -> left != NULL) {
+                q.push({root -> left, line-1});
+            }
+            if(node -> right != NULL) {
+                q.push({root -> right, line+1});
+            }
+        }
+        
+        for(auto it:mp){
+          ans.push_back(it.second);  
+        }
+        return ans;
+        */
     }
 };
 
