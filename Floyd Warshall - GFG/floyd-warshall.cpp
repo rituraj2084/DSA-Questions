9797fd2f6e55aaac05c6a34771c5f26fa822fc9e
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-07-16 04:20:27 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.cpp
+++ Floyd Warshall - GFG/floyd-warshall.cpp
@@ -12,6 +12,24 @@ class Solution {
 	void shortest_distance(vector<vector<int>>&matrix){
 	    // Code here
 	    int n = matrix.size();
+	    
+	    for(int k=0; k<n; k++){
+	        for(int u=0; u<n; u++){
+	            for(int v=0; v<n; v++){
+	                if(matrix[u][k] != -1 and matrix[k][v] != -1){
+	                    if(matrix[u][v] == -1){
+	                        matrix[u][v] = matrix[u][k] + matrix[k][v];
+	                    }
+	                    else{
+	                        matrix[u][v] = min(matrix[u][v], matrix[u][k]+matrix[k][v]);
+	                    }
+	                }
+	            }
+	        }
+	    }
+	}
+	    /*
+	    int n = matrix.size();
 	   
 	 
 	   for(int k=0;k<n;k++)
@@ -29,6 +47,7 @@ class Solution {
 	       }
 	   }
 	}
+	*/
 };
 
 // { Driver Code Starts.
