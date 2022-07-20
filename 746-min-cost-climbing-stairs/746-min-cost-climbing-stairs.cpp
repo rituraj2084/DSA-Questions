c44fcdb10c96e0ab916dc5c5bbc5d3f9b10e8ee6
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-07-20 23:40:32 +0530

    Time: 13 ms (35.56%), Space: 13.8 MB (25.37%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -13,11 +13,26 @@ public:
         dp[n] = cost[n] + min(solve2(cost, n-1, dp), solve2(cost, n-2, dp));
         return dp[n];
     }
+    int solve3(vector<int>&cost, int n){
+        vector<int>dp(n+1);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for(int i = 2; i < n; i++){
+            dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+        }
+        return min(dp[n-1], dp[n-2]);
+    }
     int minCostClimbingStairs(vector<int>& cost) {
+        //tabulation
         int n = cost.size();
-        vector<int>dp(n+1, -1);
-        int ans = min(solve2(cost, n-1, dp), solve2(cost, n-2, dp));
-        return ans;
+        return solve3(cost, n);
+        
+        //memoization
+        // int n = cost.size();
+        // vector<int>dp(n+1, -1);
+        // int ans = min(solve2(cost, n-1, dp), solve2(cost, n-2, dp));
+        // return ans;
         /*
         //recursive
         int n = cost.size();
