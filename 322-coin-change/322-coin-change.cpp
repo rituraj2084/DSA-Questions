47d60ff0c46c623fe7442eb90ab53c0b1ff9f5e6
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-07-21 22:45:45 +0530

    Time: 193 ms (23.74%), Space: 14.4 MB (59.03%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-    int solveMem(vector<int>& coins, int amount, vector<int>&dp){
-        if(amount == 0) return 0;
-        if(amount < 0) return INT_MAX;
-        int mini = INT_MAX;
-        if(dp[amount] != -1) return dp[amount];
-        for(int i = 0; i < coins.size(); i++){
-            int ans = solveMem(coins, amount - coins[i], dp);
-            if(ans != INT_MAX) mini = min(mini, ans+1);
-        }
-        dp[amount] = mini;
-        return dp[amount];
-    }
     int solveRec(vector<int>&coins, int amount){
         if(amount == 0) return 0;
         if(amount < 0) return INT_MAX;
@@ -22,13 +10,41 @@ public:
         }
         return mini;
     }
+     int solveMem(vector<int>& coins, int amount, vector<int>&dp){
+        if(amount == 0) return 0;
+        if(amount < 0) return INT_MAX;
+        int mini = INT_MAX;
+        if(dp[amount] != -1) return dp[amount];
+        for(int i = 0; i < coins.size(); i++){
+            int ans = solveMem(coins, amount - coins[i], dp);
+            if(ans != INT_MAX) mini = min(mini, ans+1);
+        }
+        dp[amount] = mini;
+        return dp[amount];
+        //return mini;
+    }
+    int solveTab(vector<int>&coins, int amount){
+        vector<int>dp(amount+1, INT_MAX);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++){
+            for(int j = 0; j < coins.size(); j++){
+                if(i - coins[j] >= 0 && dp[i - coins[j]] != INT_MAX){
+                    dp[i] = min(dp[i], 1+dp[i-coins[j]]);
+                }
+            }
+        }
+        if(dp[amount] == INT_MAX) return -1;
+        return dp[amount];
+    }
     int coinChange(vector<int>& coins, int amount) {
+        //tabulation
+        return solveTab(coins, amount);
+        
         //memoization
-        int n = coins.size();
-        vector<int>dp(amount+1, -1);
-        int ans = solveMem(coins, amount, dp);
-        if(ans == INT_MAX) return -1;
-        return ans;
+        // vector<int>dp(amount+1, -1);
+        // int ans = solveMem(coins, amount, dp);
+        // if(ans == INT_MAX) return -1;
+        // return ans;
         
         //recursive
         // int ans = solveRec(coins, amount);
