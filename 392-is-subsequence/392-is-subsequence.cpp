32c310f5188e08107a78a43cdd2e5c05b5e88f0b
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-07-25 19:24:11 +0530

    Time: 5 ms (28.30%), Space: 8.9 MB (6.35%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,19 +1,57 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-      if(s.length() > t.length()) 
-          return false;
-        int i = 0, j = 0;
-        while(i < s.length() && j < t.length()){
-            if(s[i] == t[j]){
-                i++;
-                j++;
-            }
-            else{
-                j++;
-            }
+    int solveRec(string &s, string &t, int i, int j){
+        if(i == 0 || j == 0){
+            return 0;
         }
-        if(i == s.length()) return true;
-        return false;
+        if(s[i-1] == t[j-1]){
+            return 1 + solveRec(s, t, i-1, j-1);
+        }
+        else{
+            return solveRec(s, t, i, j-1);
+        }
+    }
+    int solveMem(string &s, string &t, int i, int j, vector<vector<int>>&dp){
+        if(i == 0 || j == 0){
+            return 0;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s[i-1] == t[j-1]){
+            dp[i][j] = 1+solveMem(s, t, i-1, j-1, dp);
+        }
+        else{
+            dp[i][j] = solveMem(s, t, i, j-1, dp);
+        }
+        return dp[i][j];
+    }
+    bool isSubsequence(string s, string t) {
+         int m = s.length(), n = t.length();
+        vector<vector<int>>dp(m+1, vector<int>(n+1, -1));
+       
+        int ans = solveMem(s, t, m, n, dp);
+        return (ans == m);
+        
+        // //recursive
+        // int m = s.length(), n = t.length();
+        // int ans = solveRec(s, t, m, n);
+        // return (ans == m);
+        
+      //   // two-pointer approach
+      // if(s.length() > t.length()) 
+      //     return false;
+      //   int i = 0, j = 0;
+      //   while(i < s.length() && j < t.length()){
+      //       if(s[i] == t[j]){
+      //           i++;
+      //           j++;
+      //       }
+      //       else{
+      //           j++;
+      //       }
+      //   }
+      //   if(i == s.length()) return true;
+      //   return false;
     }
 };
