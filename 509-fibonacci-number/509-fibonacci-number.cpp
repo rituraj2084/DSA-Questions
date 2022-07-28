64354d9bbaaeac4defad494b77cd5648326c34b6
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-07-28 22:26:41 +0530

    Time: 26 ms (11.14%), Space: 5.8 MB (78.84%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,7 +1,10 @@
 class Solution {
 public:
-    int fib(int n) {
-       if(n <= 1) return n;
+    int solve(int n){
+        if(n <= 1) return n;
         return fib(n-1) + fib(n-2);
+    }
+    int fib(int n) {
+       return solve(n);
     }
 };
