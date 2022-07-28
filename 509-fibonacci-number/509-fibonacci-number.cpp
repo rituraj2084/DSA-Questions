fa16b54faf98bd0b8b83609c92e5bc3eb5859311
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-07-28 22:30:10 +0530

    Time: 5 ms (43.01%), Space: 6.2 MB (8.59%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,9 +2,22 @@ class Solution {
 public:
     int solve(int n){
         if(n <= 1) return n;
-        return fib(n-1) + fib(n-2);
+        return solve(n-1) + solve(n-2);
     }
+    
+    int solveMem(int n, vector<int> &dp){
+        if(n <= 1) return n;
+        if(dp[n] != -1) return dp[n];
+        dp[n] = solveMem(n-1, dp) + solveMem(n-2, dp);
+        return dp[n];
+    }
+    
     int fib(int n) {
-       return solve(n);
+        //Memoization
+        vector<int>dp(n+1, -1);
+        return solveMem(n, dp);
+        
+        // recursive
+       //return solve(n);
     }
 };
