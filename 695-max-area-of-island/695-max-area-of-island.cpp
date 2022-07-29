3159763f9173a8b18911df8be0ef15d90cb5b4d0
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-07-30 02:30:38 +0530

    Time: 33 ms (41.10%), Space: 23.3 MB (61.73%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -8,11 +8,35 @@ public:
         return max_area;
     }
     
+    int dfs(vector<vector<int>> &grid, int i, int j){
+        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[i].size() || grid[i][j] == 0){
+            return 0;
+        }
+        grid[i][j] = 0;
+        int count = 1;
+        count += dfs(grid, i-1, j);
+        count += dfs(grid, i+1, j);
+        count += dfs(grid, i, j-1);
+        count += dfs(grid, i, j+1);
+        return count;
+    }
+    
     int AreaOfIsland(vector<vector<int>>& grid, int i, int j){
+        int maxi = 0;
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[i].size(); j++){
+                if(grid[i][j] == 1){
+                    maxi = max(maxi, dfs(grid, i, j));
+                }
+            }
+        }
+        return maxi;
+        /*
         if( i >= 0 && i < grid.size() && j >= 0 && j < grid[0].size() && grid[i][j] == 1){
             grid[i][j] = 0;
             return 1 + AreaOfIsland(grid, i+1, j) + AreaOfIsland(grid, i-1, j) + AreaOfIsland(grid, i, j-1) + AreaOfIsland(grid, i, j+1);
         }
         return 0;
+        */
     }
 };
