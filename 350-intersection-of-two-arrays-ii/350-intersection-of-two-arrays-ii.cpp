a3e29693040094efe1fd9bbebc72c1e208e7e2d3
rituraj2084 <78496186+rituraj2084@users.noreply.github.com>
2022-07-30 17:04:09 +0530

    Time: 12 ms (35.39%), Space: 10.8 MB (7.66%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int>freq;
+        vector<int>ans;
+        for(int i = 0;i<nums1.size();i++){
+            freq[nums1[i]]++;
+        }
+        for(int i = 0;i<nums2.size();i++){
+            if (freq[nums2[i]] > 0){
+                freq[nums2[i]]--; 
+                ans.push_back(nums2[i]);
+            }
+        }
+        return ans;
+        /*
         vector<int> ans;
         sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
@@ -19,5 +32,7 @@ public:
             }
         }
         return ans;
+        */
+        
     }
 };
