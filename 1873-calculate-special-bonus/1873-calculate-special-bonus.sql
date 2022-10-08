e41f9f4f5cdb9166cccf9d4c58fe73ab6afc40dc
Ritu Raj <78496186+rituraj2084@users.noreply.github.com>
2022-10-08 19:35:21 +0530

    Time: 838 ms (37.39%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -13,12 +13,3 @@ end
 as bonus
 from Employees
 order by employee_id
-
-
-# select employee_id , 
-#    case when employee_id%2 <>0 and name not like 'M%' then salary 
-#    else 0
-#    end 
-#    as bonus
-#    from employees
-#    order by employee_id;
