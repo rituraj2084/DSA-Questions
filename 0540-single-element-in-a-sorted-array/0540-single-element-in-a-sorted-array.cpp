a26bce225d147ae9830ab357d1193a93b2b39113
Ritu Raj <78496186+rituraj2084@users.noreply.github.com>
2023-07-22 19:52:33 +0530

    Time: 32 ms (45.24%), Space: 22.3 MB (30.22%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,10 +1,38 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int ans = 0;
-        for(int i = 0; i < nums.size(); i++){
-            ans = ans^nums[i];
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            bool evenPairs = (right - mid) % 2 == 0;
+
+            if (nums[mid] == nums[mid + 1]) {
+                if (evenPairs) {
+                    left = mid + 2; // Single element lies on the right side
+                } else {
+                    right = mid - 1; // Single element lies on the left side
+                }
+            } else if (nums[mid] == nums[mid - 1]) {
+                if (evenPairs) {
+                    right = mid - 2; // Single element lies on the left side
+                } else {
+                    left = mid + 1; // Single element lies on the right side
+                }
+            } else {
+                // nums[mid] is the single element
+                return nums[mid];
+            }
         }
-        return ans;
+
+        // At this point, left and right will be pointing to the single element
+        return nums[left];
+        
+        // int ans = 0;
+        // for(int i = 0; i < nums.size(); i++){
+        //     ans = ans^nums[i];
+        // }
+        // return ans;
     }
 };
