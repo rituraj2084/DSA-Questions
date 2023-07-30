6267cf2e4674d289b2d006baf63e4ab290723a6a
Ritu Raj <78496186+rituraj2084@users.noreply.github.com>
2023-07-30 09:50:02 +0530

    Time: 30 ms (82.41%), Space: 9.5 MB (43.21%) - LeetHub

--- 0664-strange-printer/0664-strange-printer.cpp
+++ 0664-strange-printer/0664-strange-printer.cpp
@@ -23,8 +23,24 @@ public:
     int strangePrinter(string s) {
         
         int n = s.length();
-        vector<vector<int>> memo(n, vector<int>(n, -1));
-        return countTurnsMemoized(s, 0, n - 1, memo);
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = dp[i][j - 1] + 1;
+                for (int k = i; k < j; k++) {
+                    if (s[k] == s[j])
+                        dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j - 1]);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+        
+        // int n = s.length();
+        // vector<vector<int>> memo(n, vector<int>(n, -1));
+        // return countTurnsMemoized(s, 0, n - 1, memo);
         
         // return countTurnsRecursive(s, 0, s.length() - 1);
     }
