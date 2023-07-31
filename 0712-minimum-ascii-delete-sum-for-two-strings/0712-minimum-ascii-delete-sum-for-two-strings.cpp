7c8d58192ca2304a17ea03a2276e99f9cd362fef
Ritu Raj <78496186+rituraj2084@users.noreply.github.com>
2023-07-31 10:23:29 +0530

    Time: 33 ms (84.48%), Space: 15.3 MB (14.89%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -47,8 +47,38 @@ public:
         return memo[key];
     }
     int minimumDeleteSum(string s1, string s2) {
-        unordered_map<string, int> memo;
-        return minimumDeleteSumHelperMemo(s1, s2, 0, 0, memo);
+        int m = s1.length();
+        int n = s2.length();
+
+        // Create a 2D vector to store the minimum ASCII sum of deleted characters
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        // Fill the first row and first column with cumulative ASCII sums of characters
+        for (int i = 1; i <= m; ++i) {
+            dp[i][0] = dp[i - 1][0] + s1[i - 1];
+        }
+
+        for (int j = 1; j <= n; ++j) {
+            dp[0][j] = dp[0][j - 1] + s2[j - 1];
+        }
+
+        // Fill the dp array to calculate the minimum ASCII sum of deleted characters
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(dp[i - 1][j] + s1[i - 1], dp[i][j - 1] + s2[j - 1]);
+                }
+            }
+        }
+
+        // Return the final result
+        return dp[m][n];
+        
+        
+        // unordered_map<string, int> memo;
+        // return minimumDeleteSumHelperMemo(s1, s2, 0, 0, memo);
         
         // return minimumDeleteSumHelper(s1, s2, 0, 0);
     }
