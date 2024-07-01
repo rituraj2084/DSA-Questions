c68a3f198c0b428f546c81e2d7caaff23ef17eec
Ritu Raj <78496186+rituraj2084@users.noreply.github.com>
2024-07-01 10:33:55 +0530

    Time: 3 ms (60.88%), Space: 10.6 MB (45.76%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -4,13 +4,7 @@ public:
         int n = arr.size();
         if(n < 3) return false;
         for(int i = 2; i < n; i++){
-            if(arr[i]%2 != 0){
-                if(arr[i-1]%2 != 0){
-                    if(arr[i-2]%2 != 0){
-                        return true;
-                    }
-                }
-            }
+            if((arr[i]%2 != 0) && (arr[i-1]%2 != 0) && (arr[i-2]%2 != 0)) return true;
         }
         return false;
     }
