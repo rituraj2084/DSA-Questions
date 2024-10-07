16ccd63967830dfbeddf98627aff8792c24676aa
Ritu Raj <78496186+rituraj2084@users.noreply.github.com>
2024-10-07 21:41:31 +0530

    Time: 653 ms (15.66%), Space: 287.6 MB (6.72%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,46 +1,40 @@
 class Solution {
 public:
-    void merge(vector<int>& nums, int low, int mid, int high) {
-        vector<int> temp;
-        int left = low;
-        int right = mid + 1;
-        
-        while (left <= mid && right <= high) {
-            if (nums[left] <= nums[right]) {
+    void merge(vector<int>&nums, int low, int mid, int high){
+        vector<int>temp;
+        int left = low, right = mid+1;
+        while(left <= mid && right <= high){
+            if(nums[left] <= nums[right]){
                 temp.push_back(nums[left]);
                 left++;
-            } else {
+            }
+            else{
                 temp.push_back(nums[right]);
                 right++;
             }
         }
-        
-        while (left <= mid) {
+        while(left <= mid){
             temp.push_back(nums[left]);
             left++;
         }
-        
-        while (right <= high) {
+        while(right <= high){
             temp.push_back(nums[right]);
             right++;
         }
-        
-        for (int i = low; i <= high; i++) {
-            nums[i] = temp[i - low];
+        for(int i = low; i <= high; i++){
+            nums[i] = temp[i-low];
         }
     }
-
-    
-    void mergeSort(vector<int>& nums, int low, int high) {
-        if (low >= high) return;
-        int mid = low + (high - low) / 2; 
-        mergeSort(nums, low, mid); 
-        mergeSort(nums, mid + 1, high); 
-        merge(nums, low, mid, high); 
+    void mergeSort(vector<int>&nums, int low, int high){
+        if(low >= high) return;
+        int mid = (low+high)/2;
+        mergeSort(nums, low, mid);
+        mergeSort(nums, mid+1, high);
+        merge(nums, low, mid, high);
     }
-
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(nums, 0, nums.size() - 1);
+        int n = nums.size();
+        mergeSort(nums, 0, n-1);
         return nums;
     }
 };
